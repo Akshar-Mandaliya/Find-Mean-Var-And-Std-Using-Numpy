6d160a8545a7963a698a9d8a082a3bbc032e0697
Mandaliya Akshar 😀😀 <mandaliyaakshar99@gmail.com>
2024-12-16 16:10:05 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,9 @@
+import numpy as np
+array = np.array([10, 20, 30, 40, 50])
+mean = np.mean(array)
+variance = np.var(array)
+std_deviation = np.std(array)
+print("Array:", array)
+print("Mean:", mean)
+print("Variance:", variance)
+print("Standard Deviation:", std_deviation)
